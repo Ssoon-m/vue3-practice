6e275e1a2bee0704eab21f24f5d4a0990dfb9afc
순민 <tkksm2@gmail.com>
2022-04-26 16:34:03 +0900

    Feat: pinia test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="greetings">
-    <h1 class="green">{{ message }}</h1>
-    <h2>{{ count }}</h2>
+    <h1 @click="counterStore.increment" class="green">{{ message }}</h1>
+    <h2>{{ countState }}</h2>
     <h3>
       You’ve successfully created a project with
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
@@ -11,19 +11,41 @@
 </template>
 
 <script lang="ts">
-import { ref, toRefs, toRef } from "vue";
+import { ref, toRefs, toRef, onMounted, computed } from "vue";
+
+import { useCounterStore } from "../stores/counter";
+
 export default {
   props: {
     msg: String,
   },
   setup(props) {
+    onMounted(() => {
+      console.log("mounted~~~~");
+    });
+    // counter 를 구조분해시에 store state의 반응성을 잃는다.
+    const counterStore = useCounterStore();
+    // 만약 useCounterStore저네 말고 기존과 같이 computed를 사용할때
+    const countState = computed(() => counterStore.doubleCount);
+    // setup에 {msg} 이렇게 인자로 받는건 안된다.
+    // 구조분해시 반응성을 유지하려면 toRefs 사용
+    // 만약 store 에서 사용하고 싶으면 storeToRefs
     const { msg } = toRefs(props);
-
-    const count = ref(0);
     const message = msg.value;
+
+    const count = ref(10);
+    const computedCount = computed(() => count.value * 2);
+    const increment = () => {
+      count.value++;
+    };
+
     return {
       count,
       message,
+      increment,
+      computedCount,
+      counterStore,
+      countState,
     };
   },
 };
